c254fc1fb082378ef63d4c53021f61fde0c02cac
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-03-03 16:36:18 +0530

    add image tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Footer = () => {
   return (
       <div className='py-5 footer-bg position-relative'>
@@ -7,13 +7,12 @@ const Footer = () => {
           <img className='position-absolute footer-right-image' src="/assets/images/png/footer-right-image.png" alt="right-image" />
           <div className="container">
               <h2 className='footer-heading text-center pb-4'>NEKOZUMA</h2>
-              <div className="d-flex align-items-center justify-content-center gap-4">
-                  <img src="/assets/images/svg/discord.svg" alt="discord" />
-                  <img src="/assets/images/svg/footer-twitter.svg" alt="twitter" />
+        <div className="d-flex align-items-center justify-content-center gap-4">
+          <a href="https://discord.com/channels"> <Image src="/assets/images/svg/discord.svg" alt="discord" width={32} height={32} /></a>
+          <a href="https://x.com/?lang=en"> <Image src="/assets/images/svg/footer-twitter.svg" alt="twitter" width={32} height={32} /></a>
               </div>
           </div>
     </div>
   )
 }
-
 export default Footer
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,69 +1,70 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Team = () => {
     return (
-        <div>
+        <div className='pb-28'>
             <div className="container">
                 <h2 className='story-heading text-center'>Team</h2>
                 <div className='d-flex md:gap-5 gap-4'>
-                <div className="d-flex flex-wrap gap-4 max-w-880 align-items-center justify-content-end">
-                    <div>
-                        <img className='team-image' src="/assets/images/png/team-image1.png" alt="image-one" />
-                        <h2 className='card-heading text-center'>Founder</h2>
-                        <p className='card-text text-center'>Jedi Trinupab </p>
-                        <p className='card-text text-center w-full'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
+                    <div className="d-flex flex-wrap gap-4 max-w-880 mx-auto align-items-center justify-content-end">
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image1.png" alt="image1" width={202} height={202} />
+                            <h2 className='card-heading text-center'>Founder</h2>
+                            <p className='card-text text-center'>Jedi Trinupab </p>
+                            <p className='card-text text-center w-full'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
                         </div>
                         <div>
-                        <img className='team-image' src="/assets/images/png/team-image2.png" alt="image-one" />
+                            <Image className='team-image' src="/assets/images/png/team-image2.png" alt="image2" width={202} height={202} />
                             <h2 className='card-heading text-center'>Co Founder</h2>
                             <p className='card-text text-center'>Suptoshi </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
-                    </div>
-                    <div>
-                        <img className='team-image' src="/assets/images/png/team-image3.png" alt="image-one" />
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image3.png" alt="image3" width={202} height={202} />
                             <h2 className='card-heading text-center'>Artist</h2>
                             <p className='card-text text-center'>Korn </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
-                    </div>
-                    <div>
-                        <img className='team-image' src="/assets/images/png/team-image4.png" alt="image-one" />
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image4.png" alt="image4" width={202} height={202} />
                             <h2 className='card-heading text-center'>Development</h2>
                             <p className='card-text text-center'>Karmic Labs  </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
                     </div>
-                </div>
-                <div className="d-flex  flex-wrap gap-4 max-w-880 align-items-center justify-content-start">
-                    <div>
-                        <img className='team-image' src="/assets/images/png/team-image5.png" alt="image-one" />
+                    <div className="d-flex flex-wrap gap-4 max-w-880 mx-auto align-items-center justify-content-start">
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image5.png" alt="image5" width={202} height={202} />
                             <h2 className='card-heading text-center'>Position</h2>
-                        <p className='card-text text-center'>Jedi Trinupab </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
-                    </div> <div>
-                        <img className='team-image' src="/assets/images/png/team-image6.png" alt="image-one" />
+                            <p className='card-text text-center'>Jedi Trinupab </p>
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image6.png" alt="image6" width={202} height={202} />
                             <h2 className='card-heading text-center'>Position</h2>
-                        <p className='card-text text-center'>Jedi Trinupab </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
-                    </div>
-                    <div>
-                        <img className='team-image' src="/assets/images/png/team-image7.png" alt="image-one" />
+                            <p className='card-text text-center'>Jedi Trinupab </p>
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image7.png" alt="image7" width={202} height={202} />
                             <h2 className='card-heading text-center'>Position</h2>
-                        <p className='card-text text-center'>Jedi Trinupab </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
-                    </div>
-                    <div>
-                        <img className='team-image' src="/assets/images/png/team-image8.png" alt="image-one" />
+                            <p className='card-text text-center'>Jedi Trinupab </p>
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
+                        <div>
+                            <Image className='team-image' src="/assets/images/png/team-image8.png" alt="image8"  width={202} height={202} />
                             <h2 className='card-heading text-center'>Position</h2>
                             <p className='card-text text-center'>Jedi Trinupab  </p>
-                        <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
-                        <img className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" />
-                    </div>
+                            <p className='card-text text-center'>However at every junction in time, there is always a darkness that is waiting to overthrow </p>
+                            <Image className='text-center mx-auto twitter d-flex align-items-center justify-content-center' src="/assets/images/svg/twitter.svg" alt="twitter" width={20} height={20} />
+                        </div>
                     </div>
                 </div>
             </div>
